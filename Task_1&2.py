311d5763d9ab6493bfc69cc49cfe990748ebf24e
pratikguptaofficial <pg26032005@gmail.com>
2025-07-22 23:15:55 +0530

    Add files via upload

---
+++ Task_1&2.py
@@ -0,0 +1,25 @@
+'''num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("\nAddition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+'''
+
+# Ask user to input first and last name
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Concatenate full name
+full_name = first_name + " " + last_name
+
+# Print personalized greeting
+print(f"\nHello, {full_name}! Welcome to the Python program.")
+
+
