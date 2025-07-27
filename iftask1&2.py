8debbf280a02c5e9e0a249db05bccc875b0928e9
pratikguptaofficial <pg26032005@gmail.com>
2025-07-27 17:34:25 +0530

    Add files via upload

---
+++ iftask1&2.py
@@ -0,0 +1,16 @@
+#num=int(input('Enter a First Number: '))
+
+#if num % 2==0:
+ #   print('Even')
+#else:
+ #   print('Odd')
+
+count = 0
+
+for i in range(1,51):
+     count += i
+print('The sum of Numbers from 1 to 50 is:',count)
+
+
+
+
